c9894bb05220f5eaa525887e018c9662e397439a
Julian <julianlx@hotmail.com>
2021-02-13 22:48:08 -0500

    Fixing pep8

--- models/base_model.py
+++ models/base_model.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
+""" This is the super class """
 
 import uuid
 from datetime import datetime
 
+
 class BaseModel():
+    """ Base class BaseModel. """
 
     def __init__(self, *args, **kwargs):
         if kwargs:
             for key in kwargs:
-                if key != "__class__" and key != "created_at" and key != "updated_at":
+                if (key != "__class__" and key != "created_at"
+                        and key != "updated_at"):
                     setattr(self, key, kwargs[key])
                 if key == "created_at" or key == "updated_at":
                     d = datetime.strptime(kwargs[key], '%Y-%m-%dT%H:%M:%S.%f')
@@ -20,12 +24,16 @@ class BaseModel():
             self.updated_at = d.isoformat()
 
     def save(self):
+        """ Method to save and update new instances """
         u = datetime.now()
         self.updated_at = u.isoformat()
 
     def to_dict(self):
+        """ Method to return a dictionary of the instaces """
         self.__dict__['__class__'] = type(self).__name__
         return self.__dict__
 
     def __str__(self):
-        return ("[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__))
+        """" Return a string representation of the instance """
+        return ("[{}] ({}) {}"
+                .format(type(self).__name__, self.id, self.__dict__))
