88609db8559fcccc278ca5c79ce5121381868db1
Julian <julianlx@hotmail.com>
2021-02-15 18:23:46 -0500

    class city

--- models/city.py
+++ models/city.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-""" a class User that inherits from BaseModel """
+""" a class City that inherits from BaseModel """
+from base_model import BaseModel
 
 
-class City:
-    pass
+class City(BaseModel):
+    """ a class City that inherits from BaseModel """
+    state_id = ''
+    name = ''
