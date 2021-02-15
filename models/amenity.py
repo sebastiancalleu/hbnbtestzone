196f1e10d51e73db645d39c11e5084b31b666bcc
Julian <julianlx@hotmail.com>
2021-02-15 18:30:13 -0500

    class amenity

---
+++ models/amenity.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" a class amenity that inherits from BaseModel """
+from base_model import BaseModel
+
+
+class amenity(BaseModel):
+    """ a class amenity that inherits from BaseModel """
+
+    name = ''
